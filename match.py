c0b0ff65278d8e2a7cc8499cc5341e6516503a28
Anton Loss <optimbid@gmail.com>
2018-10-26 13:29:45 +0300

    task

---
+++ match.py
@@ -0,0 +1,45 @@
+import json
+
+
+def main():
+    with open('./users.json') as f:
+        users = json.load(f)
+
+    with open('./venues.json') as f:
+        venues = json.load(f)
+
+    for venue in venues:
+        food_venue = set(venue["food"])
+        drink_venue = set(venue["drinks"])
+
+        venue["picky_eaters"] = []
+        venue["picky_drinkers"] = []
+
+        for user in users:
+            food_user = set(user["wont_eat"])
+            drink_user = set(user["drinks"])
+
+            if len(food_venue - food_user) == 0:
+                venue["picky_eaters"].append(user["name"])
+
+            if len(drink_venue & drink_user) == 0:
+                venue["picky_drinkers"].append(user["name"])
+
+    print("Places to go:")
+    for venue in venues:
+        if venue["picky_eaters"] or venue["picky_drinkers"]:
+            continue
+        print(f"* {venue['name']}")
+
+    print("Places to avoid:")
+    for venue in venues:
+        if venue["picky_eaters"] or venue["picky_drinkers"]:
+            print(f"* {venue['name']}")
+        for eater in venue["picky_eaters"]:
+            print(f"\t * There's nothing for {eater} to eat")
+        for drinker in venue["picky_drinkers"]:
+            print(f"\t * There's nothing for {drinker} to drink")
+
+
+if __name__ == "__main__":
+    main()
